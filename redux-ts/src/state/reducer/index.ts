12f5a90c170872ad78cedec8a1d54ac26a31dd53
Dhananjay Sakhare <dsagm7>
2023-10-27 17:24:16 +0530

    proper use of useSelector with ts

--- redux-ts/src/state/reducer/index.ts
+++ redux-ts/src/state/reducer/index.ts
@@ -4,3 +4,5 @@ import reducer from "./repositoriesReducer";
 const reducers = combineReducers({ repositories: reducer });
 
 export default reducers;
+
+export type RootState = ReturnType<typeof reducers>;
