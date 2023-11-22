e6c3c670ed37a14e6126074541c3d179b427cc59
Dhananjay Sakhare <dsagm7>
2023-11-22 11:46:46 +0530

    setting up wasm service

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    console.log(service);
+  };
   const onClick = () => {
     console.log(input);
   };
